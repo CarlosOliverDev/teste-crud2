b4966a94c3ac04570605656c8f7b0621c273629b
Carlos <carlos.oli.h23@gmail.com>
2026-02-26 20:48:19 -0300

    Programa finalizado.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import entities.Usuario;
+import repository.UsuarioRepository;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,10 +14,59 @@ public class Main {
     private static String senha = new Senha().getSenha();
 
     public static void main(String[] args) {
+        Usuario usuario = new Usuario(0, "Carlos Oliveira", "carlos.teste@gmail.com");
+        Usuario usuario2 = new Usuario(0, "João Silva", "joao@email.com");
+        Usuario usuario3 = new Usuario(0, "Jordana", "jordana@email.com");
+        Usuario usuario4 = new Usuario(0, "Maria", "maria@email.com");
+        Usuario usuario5 = new Usuario(0, "Miguel", "miguel@email.com");
+
         System.out.println("Estabelecendo conexão com o Banco de Dados...");
         try(Connection connection = DriverManager.getConnection(URL,user,senha)) {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             System.out.println("Conectado ao Banco de Dados!");
+
+            UsuarioRepository usuarioRepository = new UsuarioRepository();
+
+            System.out.println("Adicionando Usuários.");
+
+            usuarioRepository.salvarUsuario(connection, usuario);
+            usuarioRepository.salvarUsuario(connection, usuario2);
+            usuarioRepository.salvarUsuario(connection, usuario3);
+            usuarioRepository.salvarUsuario(connection, usuario4);
+            usuarioRepository.salvarUsuario(connection, usuario5);
+
+            System.out.println("Usuários adicionados com sucesso.");
+
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Thread.sleep(1000);
+
+            System.out.println("Excluindo Usuário.");
+
+            usuarioRepository.excluirUsuarioPeloNome(connection, usuario2.getNome());
+
+            System.out.println("Usuário removido com sucesso.");
+
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Thread.sleep(1000);
+
+            System.out.println("Atualizando email do Usuário.");
+
+            String novoEmail = "carlos.teste123@email.com";
+            usuarioRepository.atualizarEmailUsuario(connection, novoEmail, usuario.getNome());
+
+            System.out.println("Usuário atualizado com sucesso.");
+
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Thread.sleep(1000);
+
+            System.out.println("Consultando todos os Usuários.");
+
+            usuarioRepository.lerTodosUsuarios(connection);
+
+            System.out.println("Consulta feita com sucesso.");
         } catch (InterruptedException | SQLException e) {
             e.printStackTrace();
             System.out.println("Não foi possível conectar com o Banco de Dados.");
--- src/repository/UsuarioRepository.java
+++ src/repository/UsuarioRepository.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioRepository {
-    public void salvarUsuario(Connection connection, Usuario usuario) {
+    public void salvarUsuario(Connection connection, Usuario usuario) throws SQLException {
         String sql = "INSERT INTO usuarios(nome, email) VALUES(?,?)";
 
         try(PreparedStatement stat = connection.prepareStatement(sql)) {
@@ -22,12 +22,11 @@ public class UsuarioRepository {
                 System.out.println("Não foi possível salvar esse Usuário.");
             }
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Falha na persistência do Usuário.");
+            throw new SQLException("Falha na persistência do Usuário.");
         }
     }
 
-    public void lerTodosUsuarios(Connection connection) {
+    public void lerTodosUsuarios(Connection connection) throws SQLException {
         String sql = """
                 SELECT id, nome, email
                 FROM usuarios
@@ -41,16 +40,15 @@ public class UsuarioRepository {
                 int id = rs.getInt("id");
                 String nome = rs.getString("nome");
                 String email = rs.getString("email");
-                System.out.printf("ID: %d - Nome: %s - Email: %s",id,nome,email);
+                System.out.printf("ID: %d - Nome: %s - Email: %s\n",id,nome,email);
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Não foi possível listar os Usuários salvos.");
+            throw new SQLException("Não foi possível listar os Usuários salvos.");
         }
     }
 
-    public void atualizarEmailUsuario(Connection connection,  String email, String nome) {
+    public void atualizarEmailUsuario(Connection connection,  String email, String nome) throws SQLException {
         String sql = """
                 UPDATE usuarios
                 SET email = ?
@@ -69,12 +67,11 @@ public class UsuarioRepository {
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Falha na atualização do Usuário.");
+            throw new SQLException("Falha na atualização do Usuário.");
         }
     }
-    
-    public void excluirUsuarioPeloNome(Connection connection, String nome) {
+
+    public void excluirUsuarioPeloNome(Connection connection, String nome) throws SQLException {
         String sql = """
                 DELETE 
                 FROM usuarios 
@@ -92,8 +89,7 @@ public class UsuarioRepository {
                 System.out.println("Não foi possível excluir esse Usuário.");
             }
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Não foi possível excluir um Usuário com esse nome.");
+            throw new SQLException("Não foi possível excluir um Usuário com esse nome.");
         }
     }
 }
