1dccc64e5daae58ca77e1fde5ebc6acc8dc6fc1d
Carlos <carlos.oli.h23@gmail.com>
2026-02-26 19:49:14 -0300

    Classes bases de Usuário.

---
+++ src/entities/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class UsuarioDTO {
+    private String nome;
+    private String email;
+
+    public UsuarioDTO(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
